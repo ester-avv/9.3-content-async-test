1a44efb0aeab5c5211e795aab194020cce7be048
Ester Viana Verza <ester.av@hotmail.com>
2022-08-18 18:17:14 -0300

    2 commit

--- tests/fetchCharacter.test.js
+++ tests/fetchCharacter.test.js
@@ -4,6 +4,6 @@ const { fetchCharacter } = require('../src/fetchCharacter');
 describe('Teste a função fetchCharacter', () => {
   it('Verifica se o nome da personagem é Wonder Woman', async () => {
     const character = await fetchCharacter ('720');
-    console.log(character);
+    
   });
 });
